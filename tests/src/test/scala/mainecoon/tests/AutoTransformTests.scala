5361746e9f098226b96340809b72d05eee70df40
Kailuo Wang <kailuo.wang@gmail.com>
2017-04-26 21:50:25 -0400

    minor reformat

--- tests/src/test/scala/mainecoon/tests/AutoTransformTests.scala
+++ tests/src/test/scala/mainecoon/tests/AutoTransformTests.scala
@@ -7,6 +7,21 @@ import scala.util.{Try, Success}
 import cats.~>
 import TransformK.ops._
 
+@autoTransform
+trait ParseAlg[F[_]] {
+  def parseInt(i: String): F[Int]
+  def floatToString(f: Float, fmt: String): F[String]
+}
+
+object Interpreters {
+  type F[T] = Try[T]
+
+  object tryParse extends ParseAlg[F] {
+    def parseInt(s: String) = Try(s.toInt)
+    def floatToString(f: Float, fmt: String): F[String] = Success(fmt + f)
+  }
+}
+
 class AutoTransformTests extends FunSuite with Matchers {
   test("can transform") {
 
@@ -17,23 +32,6 @@ class AutoTransformTests extends FunSuite with Matchers {
     optionParse.parseInt("3") should be(Some(3))
     optionParse.parseInt("sd") should be(None)
     optionParse.floatToString(3f, "d") should be(Some("d3.0"))
-  }
-}
-
-@autoTransform
-trait ParseAlg[F[_]] {
-  def parseInt(i: String): F[Int]
-  def floatToString(f: Float, fmt: String): F[String]
-}
-
-
-object Interpreters {
-  type F[T] = Try[T]
-
-  object tryParse extends ParseAlg[F] {
-    def parseInt(s: String) = Try(s.toInt)
-
-    def floatToString(f: Float, fmt: String): F[String] = Success(fmt + f)
   }
 }
 

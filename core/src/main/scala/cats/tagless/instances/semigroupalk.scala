e5fa67260a2be537e6b50618d4ac369178f807b3
pcosmin33 <pcosmin@yahoo.com>
2019-12-13 15:29:52 +0200

    deleted iso and hkinds

--- core/src/main/scala/cats/tagless/instances/semigroupalk.scala
+++ core/src/main/scala/cats/tagless/instances/semigroupalk.scala
@@ -1,19 +1,3 @@
-/*
- * Copyright 2017 Kailuo Wang
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package cats.tagless.instances
 
 import cats._
@@ -21,8 +5,6 @@ import cats.implicits._
 import cats.data.Tuple2K
 import cats.kernel.CommutativeMonoid
 import cats.tagless._
-
-object SemigroupalKInstances extends SemigroupalKInstances
 
 trait SemigroupalKInstances extends SemigroupalKInstances01 {
   implicit def semKMonad:         SemigroupalK[Monad]         = new SemKMonad {}

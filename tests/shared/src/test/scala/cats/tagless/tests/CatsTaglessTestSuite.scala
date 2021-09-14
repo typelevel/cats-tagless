a362eee9225a15694be7866826af4259e180f56a
Georgi Krastev <joro.kr.21@gmail.com>
2021-09-14 19:31:20 +0200

    Enable -Xsource:3 and remove instances which become unnecessary

--- tests/shared/src/test/scala/cats/tagless/tests/CatsTaglessTestSuite.scala
+++ tests/shared/src/test/scala/cats/tagless/tests/CatsTaglessTestSuite.scala
@@ -18,12 +18,10 @@ package cats.tagless.tests
 
 import cats.Eq
 import cats.arrow.FunctionK
-import cats.data._
+import cats.data.*
 import cats.laws.discipline.ExhaustiveCheck
 import cats.tagless.aop.Instrumentation
-import cats.tagless.laws.discipline.SemigroupalKTests.IsomorphismsK
 import cats.tagless.syntax.AllSyntax
-import cats.tagless.{InvariantK, Tuple3K}
 import munit.DisciplineSuite
 import org.scalacheck.{Arbitrary, Gen}
 
@@ -69,75 +67,6 @@ trait TestInstances {
   implicit def catsTaglessLawsEqForInstrumentation[F[_], A](implicit ev: Eq[F[A]]): Eq[Instrumentation[F, A]] =
     Eq.by(i => (i.algebraName, i.methodName, i.value))
 
-  //------------------------------------------------------------------
-  // The instances below are needed due to type inference limitations:
-  //------------------------------------------------------------------
-
-  implicit def catsTaglessLawsIsomorphismsK110[Alg[_[_], _[_], _], F[_], A](implicit
-      ev: InvariantK[Alg[F, *[_], A]]
-  ): IsomorphismsK[Alg[F, *[_], A]] = IsomorphismsK.invariantK[Alg[F, *[_], A]]
-
-  implicit def catsTaglessLawsIsomorphismsK100[Alg[_[_], _, _], A, B](implicit
-      ev: InvariantK[Alg[*[_], A, B]]
-  ): IsomorphismsK[Alg[*[_], A, B]] = IsomorphismsK.invariantK[Alg[*[_], A, B]]
-
-  implicit def catsTaglessLawsIsomorphismsK10[Alg[_[_], _], A](implicit
-      ev: InvariantK[Alg[*[_], A]]
-  ): IsomorphismsK[Alg[*[_], A]] = IsomorphismsK.invariantK[Alg[*[_], A]]
-
-  implicit def catsTaglessLawsIsomorphismsKForTuple2K1[G[_], A](implicit
-      ev: InvariantK[Tuple2K[*[_], G, A]]
-  ): IsomorphismsK[Tuple2K[*[_], G, A]] = IsomorphismsK.invariantK[Tuple2K[*[_], G, A]]
-
-  implicit def catsTaglessLawsEqForTuple2K[F[_], G[_], A](implicit
-      ev: Eq[(F[A], G[A])]
-  ): Eq[Tuple2K[F, G, A]] = Eq.by(t2k => (t2k.first, t2k.second))
-
-  implicit def catsTaglessLawsEqForEitherKTuple3K[F[_], G[_], H[_], I[_], A](implicit
-      ev: Eq[Either[F[A], (G[A], H[A], I[A])]]
-  ): Eq[EitherK[F, Tuple3K[G, H, I]#λ, A]] = Eq.by(_.run)
-
-  implicit def catsTaglessLawsEqForEitherTTuple3K[F[_], G[_], H[_], A, B](implicit
-      ev: Eq[(F[Either[A, B]], G[Either[A, B]], H[Either[A, B]])]
-  ): Eq[EitherT[Tuple3K[F, G, H]#λ, A, B]] = Eq.by(_.value)
-
-  implicit def catsTaglessLawsEqForFuncTuple3K[F[_], G[_], H[_], A, B](implicit
-      ev: Eq[A => (F[B], G[B], H[B])]
-  ): Eq[Func[Tuple3K[F, G, H]#λ, A, B]] = Eq.by(_.run)
-
-  implicit def catsTaglessLawsEqForIdTTuple3K[F[_], G[_], H[_], A](implicit
-      ev: Eq[(F[A], G[A], H[A])]
-  ): Eq[IdT[Tuple3K[F, G, H]#λ, A]] = Eq.by(_.value)
-
-  implicit def catsTaglessLawsEqForIorTTuple3K[F[_], G[_], H[_], A, B](implicit
-      ev: Eq[(F[Ior[A, B]], G[Ior[A, B]], H[Ior[A, B]])]
-  ): Eq[IorT[Tuple3K[F, G, H]#λ, A, B]] = Eq.by(_.value)
-
-  implicit def catsTaglessLawsEqForKleisliTuple3K[F[_], G[_], H[_], A, B](implicit
-      ev: Eq[A => (F[B], G[B], H[B])]
-  ): Eq[Kleisli[Tuple3K[F, G, H]#λ, A, B]] = Eq.by(_.run)
-
-  implicit def catsTaglessLawsEqForCokleisliTuple3K[F[_], G[_], H[_], A, B](implicit
-      ev: Eq[((F[A], G[A], H[A])) => B]
-  ): Eq[Cokleisli[Tuple3K[F, G, H]#λ, A, B]] = Eq.by(_.run)
-
-  implicit def catsTaglessLawsEqForOneAndTuple3K[F[_], G[_], H[_], A](implicit
-      ev: Eq[(A, (F[A], G[A], H[A]))]
-  ): Eq[OneAnd[Tuple3K[F, G, H]#λ, A]] = Eq.by(x => (x.head, x.tail))
-
-  implicit def catsTaglessLawsEqForOptionTTuple3K[F[_], G[_], H[_], A](implicit
-      ev: Eq[(F[Option[A]], G[Option[A]], H[Option[A]])]
-  ): Eq[OptionT[Tuple3K[F, G, H]#λ, A]] = Eq.by(_.value)
-
-  implicit def catsTaglessLawsEqForTuple2KTuple3K1[F[_], G[_], H[_], I[_], A](implicit
-      ev: Eq[((F[A], G[A], H[A]), I[A])]
-  ): Eq[Tuple2K[λ[a => (F[a], G[a], H[a])], I, A]] = Eq.by(t2k => (t2k.first, t2k.second))
-
-  implicit def catsTaglessLawsEqForTuple2KTuple3K2[F[_], G[_], H[_], I[_], A](implicit
-      ev: Eq[(F[A], (G[A], H[A], I[A]))]
-  ): Eq[Tuple2K[F, Tuple3K[G, H, I]#λ, A]] = Eq.by(t2k => (t2k.first, t2k.second))
-
-  implicit def catsTaglessLawsEqForWriterTTuple3K[F[_], G[_], H[_], A, B](implicit
-      ev: Eq[(F[(A, B)], G[(A, B)], H[(A, B)])]
-  ): Eq[WriterT[Tuple3K[F, G, H]#λ, A, B]] = Eq.by(_.run)
+  implicit def catsTaglessLawsEqForTuple2K[F[_], G[_], A](implicit fa: Eq[F[A]], ga: Eq[G[A]]): Eq[Tuple2K[F, G, A]] =
+    Eq.by(t2k => (t2k.first, t2k.second))
 }

dfc78f7c23425999aa09637f01808c44357374c9
A. Alonso Dominguez <2269440+alonsodomin@users.noreply.github.com>
2024-07-04 10:13:46 +0200

    Auto derive Scala 3 macro for ReaderT (#557)
    * Implementation for auto derivation of ReaderT in Scala 3
    
    * No need to change owner
    
    * Remove unused imports
    
    * Format sources
    
    * Simplify ReaderT macro
    
    ---------
    
    Co-authored-by: Georgi Krastev <joro.kr.21@gmail.com>

---
+++ core/src/main/scala-3/cats/tagless/macros/MacroReaderT.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2019 cats-tagless maintainers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cats.tagless.macros
+
+import cats.tagless.*
+import cats.data.ReaderT
+
+import scala.annotation.experimental
+import scala.quoted.*
+
+@experimental
+object MacroReaderT:
+
+  inline def derive[Alg[_[_]], F[_]]: Alg[[X] =>> ReaderT[F, Alg[F], X]] = ${ deriveReaderT }
+
+  private[macros] def deriveReaderT[Alg[_[_]]: Type, F[_]: Type](using
+      q: Quotes
+  ): Expr[Alg[[X] =>> ReaderT[F, Alg[F], X]]] =
+    import quotes.reflect.*
+    given dm: DeriveMacros[q.type] = new DeriveMacros
+
+    val RT = TypeRepr.of[ReaderT[F, Alg[F], ?]]
+    val name = Symbol.freshName("$anon")
+    val parents = List(TypeTree.of[Object], TypeTree.of[Alg[[X] =>> ReaderT[F, Alg[F], X]]])
+    val cls = Symbol.newClass(Symbol.spliceOwner, name, parents.map(_.tpe), _.overridableMembers, None)
+
+    def readerT[T: Type](owner: Symbol)(body: Term => Term): Term =
+      '{ ReaderT((af: Alg[F]) => ${ body('af.asTerm).asExprOf[F[T]] }) }.asTerm.changeOwner(owner)
+
+    def argTransformer(af: Term): dm.Transform =
+      case (_, tpe, arg) if tpe <:< RT => Select.unique(arg, "apply").appliedTo(af)
+
+    def transformDef(method: DefDef)(argss: List[List[Tree]]): Option[Term] =
+      method.returnTpt.tpe.asType match
+        case '[ReaderT[F, Alg[F], t]] =>
+          Some(readerT[t](method.symbol): af =>
+            af.call(method.symbol):
+              for (clause, args) <- method.paramss.zip(argss)
+              yield for paramAndArg <- clause.params.zip(args)
+              yield argTransformer(af).transformArg(method.symbol, paramAndArg)
+          )
+        case _ =>
+          method.rhs
+
+    def transformVal(value: ValDef): Option[Term] =
+      value.tpt.tpe.asType match
+        case '[ReaderT[F, Alg[F], t]] =>
+          Some(readerT[t](value.symbol)(_.select(value.symbol)))
+        case _ =>
+          value.rhs
+
+    val members = cls.declarations
+      .filterNot(_.isClassConstructor)
+      .map: member =>
+        member.tree match
+          case method: DefDef => DefDef(member, transformDef(method))
+          case value: ValDef => ValDef(member, transformVal(value))
+          case _ => report.errorAndAbort(s"Not supported: $member in ${member.owner}")
+
+    val newCls = New(TypeIdent(cls)).select(cls.primaryConstructor).appliedToNone
+    Block(ClassDef(cls, parents, members) :: Nil, newCls).asExprOf[Alg[[X] =>> ReaderT[F, Alg[F], X]]]
